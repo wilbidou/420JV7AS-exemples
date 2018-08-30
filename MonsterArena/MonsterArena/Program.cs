b45f9f325e8dadd73b278120d4b685d3c63e9213
yassine <yassineougouag@gmail.com>
2018-08-30 13:52:33 -0400

    addnewmonster

--- MonsterArena/MonsterArena/Program.cs
+++ MonsterArena/MonsterArena/Program.cs
@@ -13,7 +13,8 @@ namespace MonsterArena
                 new Goblin("Alice"),
                 new Orc("Bob"),
                 new Goblin("Charlie"),
-                new Orc("David")
+                new Orc("David"),
+                new Furry("Trap")
             };
 
             foreach (var monster in monsters)
