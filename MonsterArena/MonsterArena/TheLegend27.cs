817a15b38a5d814705b69acc4422d37f2b035cad
Colin <colinbougie@gmail.com>
2018-08-30 20:29:16 -0400

    Made the AI for TheLegend27
    -Colin

--- MonsterArena/MonsterArena/TheLegend27.cs
+++ MonsterArena/MonsterArena/TheLegend27.cs
@@ -2,13 +2,41 @@
 
 namespace MonsterArena
 {
-    class Goblin : Monster
+    class TheLegend27 : Monster
     {
-        public Goblin(string name) : base(name)
+        public TheLegend27(string name) : base(name)
         {
             AddBonusLuck(25);
             AddBonusStrength(60);
             AddBonusVitality(15);
         }
+		
+		public override int GetAttackIndex(List<MonsterData> monsters)
+        {
+			List<int> monsterWeight = new List<int>();
+
+			for (int i = 0; i < monsters.Count; i++)//For each monster
+			{
+				int weight = 0;
+
+				if (monsters[i].name == GetData().name)//I'm not tasty
+					weight -= 10000;
+				if (monsters[i].hp <= 0)//Dead people aren't tasty either
+					weight -= 10000;
+
+				weight -= monsters[i].hp;//The more HP, the less tasty
+				weight += (monsters[i].level - 1) * 20;//More LVL -> more tasty
+				monsterWeight.Add(weight);
+			}
+
+			int index = 0;
+			for (int i = 0; i < monsterWeight.Count; i++)
+			{
+				if (monsterWeight[i] > monsterWeight[index])
+					index = i;
+			}
+
+			return index;
+		}
     }
 }
