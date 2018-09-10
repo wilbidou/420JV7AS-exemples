d6cc70b8688a4c9aa6ef827341da17b007c79cfa
Unknown <felix@felixsoum.com>
2018-09-10 13:12:09 -0400

    Add LINQ example

---
+++ Cours03LINQ/Cours03LINQ/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Cours03LINQ
+{
+    class Program
+    {
+        const int David = 7;
+        static void Main(string[] args)
+        {
+            var goblins = new List<Goblin>()
+            {
+                new Goblin("Dragon", 11, "David"),
+                new Goblin("Orc", 20, "Adrien"),
+                new Goblin("Hob", 30, "Mark"),
+                new Goblin("Dragon", -1, "Daniel"),
+                new Goblin("Orc", 15, "Samuel"),
+                new Goblin("Hob", 30, "Colin")
+            };
+
+            var query = from goblin in goblins
+                        select new { goblin.Name, goblin.Hp };
+
+            foreach (var x in query)
+            {
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
